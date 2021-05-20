675cd8a523fc9b3f71ecf101cdd51fbade369653
VarunKota <66583960+VarunKota@users.noreply.github.com>
2021-05-20 19:19:15 +0530

    Update 09_reverse_linked_list.py

--- 09_reverse_linked_list.py
+++ 09_reverse_linked_list.py
@@ -11,3 +11,34 @@ Here's a sample linked list node class:
         self.next  = None   """
 
 # Start coding from here
+class ListNode:
+    def _init_(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        head, next = self._recursive(head)
+        _next.next = None
+        return head
+
+    def __recursive(self, head: ListNode) -> (ListNode, ListNode):
+        if not head.next:
+            return head, head
+
+        node, next = self._recursive(head.next)
+        _next.next = head
+
+        return node, head
+
+
+if _name_ == "_main_":
+    solution = Solution()
+
+    head = ListNode(1, ListNode(2))
+    head.next = ListNode(3)
+    head.next.next = ListNode(4)
+    head.next.next.next = ListNode(5)
+
+    print(solution.reverseList(head))
